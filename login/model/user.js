1f9d457f6592729efb2c0e19422a461ce35bbf05
EC2 Default User <ec2-user@ip-172-31-6-172.ec2.internal>
2022-07-19 02:32:48 +0000

    first commit

---
+++ login/model/user.js
@@ -0,0 +1,15 @@
+const crypto = require('crypto')
+
+module.exports = class User {
+  constructor(login_id, password, name) {
+    this.login_id = login_id
+    this.password = password
+    this.name = name
+  }
+
+  verifyPassword(password) {
+    const hashedPass = crypto.createHash('sha256').update(password).digest('hex')
+    console.log(hashedPass)
+    return this.password === hashedPass
+  }
+}
